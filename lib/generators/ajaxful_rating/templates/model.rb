154684c05cb71c50d1874494156640eb0a980fbb
Fabian Schwahn <fabian.schwahn@gmail.com>
2022-11-17 12:21:05 +0100

    Remove attr_accessible
    This is outdated since rails 4 😄

--- lib/generators/ajaxful_rating/templates/model.rb
+++ lib/generators/ajaxful_rating/templates/model.rb
@@ -2,6 +2,4 @@ class Rate < ApplicationRecord
   belongs_to :rater, class_name: "<%= file_name.classify %>"
   belongs_to :rateable, polymorphic: true
   validates_numericality_of :stars, minimum: 1
-
-  attr_accessible :rate, :dimension
 end
